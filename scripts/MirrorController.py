375f787a499a3a7e171528bf19503482e051b50f
yuke <96366114+y-anim16@users.noreply.github.com>
2023-08-03 01:01:29 +0900

    update 左右対称にないコントローラのミラーを行えるように ｘの移動値、y,zの回転値を―1倍

--- scripts/MirrorController.py
+++ scripts/MirrorController.py
@@ -4,7 +4,15 @@ import maya.mel as mel
 def MirrorController():
     selected_objects = cmds.ls(sl=True, dag = True, type="transform")
 
-    print(getCtrlName(selected_objects[0]))
+    for obj in selected_objects:
+        ctrlName = getCtrlName(obj)
+        print(ctrlName)
+
+        prefix = getSymmetryPrefix(ctrlName)
+        print(prefix)
+
+        if prefix == None:
+           mirrorNonSymmetryCtrl(obj)
     return
     
 def getCtrlName(obj):
@@ -15,12 +23,34 @@ def getCtrlName(obj):
         counter+=1
     return obj[-counter:]
 
-    if not selected_objects:
-        cmds.error("not selected")
-        return
-    
-    for obj in selected_objects:
+def getSymmetryPrefix(name):
+    if name[:2] == 'L_':
+        return 'R_'
+    elif name[:2] == 'R_':
+        return 'L_'
+    else:
+        return None
 
-        cmds.move(0, 0, 0, obj, localSpace = True, absolute = True)
-        cmds.rotate(0, 0, 0, obj, absolute=True)
-        cmds.scale(1, 1, 1, obj, absolute=True)
+    # if not selected_objects:
+    #     cmds.error("not selected")
+    #     return
+    
+    # for obj in selected_objects:
+
+    #     cmds.move(0, 0, 0, obj, localSpace = True, absolute = True)
+    #     cmds.rotate(0, 0, 0, obj, absolute=True)
+    #     cmds.scale(1, 1, 1, obj, absolute=True)
+
+def mirrorNonSymmetryCtrl(obj):
+        current_position = cmds.xform(obj, query=True, translation=True, objectSpace=True)
+        current_rotation = cmds.xform(obj, query=True, rotation=True, objectSpace=True)
+        print(current_rotation)
+
+        symmetric_position = list(current_position)
+        symmetric_position[0] = -current_position[0] # x axis
+
+        symmetric_rotation = list(current_rotation)
+        symmetric_rotation[1] = -current_rotation[1] # y axis
+        symmetric_rotation[2] = -current_rotation[2] # z axis
+
+        cmds.xform(obj, translation=symmetric_position, rotation=symmetric_rotation, objectSpace=True)
